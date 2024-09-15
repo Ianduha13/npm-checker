1df7fed62d7b5879f7f581bd0e2f146b30091edd
Ianduha13 <ianduhamelhayes@gmail.com>
2024-09-15 19:29:09 -0300

    feat: add posthog analytics and move footer to a component

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import { Heart } from "lucide-react";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="absolute bottom-0 w-full flex justify-center mb-4">
+      <p className="flex items-center flex-wrap justify-center">&copy; 2024, Npm Check is a free tool made with
+        <Heart className="text-[#C12127] h-5 w-5 mx-1" fill="#C12127" />  by
+        <Link className="text-[#C12127] ml-1" href="https://www.linkedin.com/in/ian-duhamel/">{" "} Ian Duhamel</Link>
+      </p>
+    </footer>
+  )
+}
